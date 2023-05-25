03d1ad3a5c0dfb87f9f85ec6f582d13fefd842af
Mario-Mellek <mario.samy266@gmail.com>
2023-05-25 17:26:35 +0000

    Testing getline()
    The code is working with no functions assigned to the Ops,
    it only detects the Ops and prints Exists if it exists
    it also skipps \n and # at the begining of the files
    
    issue :
    getline() doesn't work with the flag -std=c89
    will replace with fgets()

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 - What are the common implementations of stacks and queues
 - What are the most common use cases of stacks and queues
 - What is the proper way to use global variables
+<hr>
+ By including a null opcode and null function pointer at the end of the
+ instructions array, the program can easily iterate over the array to check
+ for valid opcodes without having to keep track
+ of the length of the array explicitly.
--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+
+
+instruction_t instructions[] = {
+	{"push", NULL},
+	{"pall", NULL},
+	{NULL, NULL}
+};
 
 int main(int argc, char **argv)
 {
 	FILE *fp = fopen(argv[1], "r");
+	size_t i, j, len = 0;
+	char *opCode, *line = NULL;
+	ssize_t read;
+	int opExists;
 
 	if (argc != 2)
 	{
@@ -16,6 +26,35 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		return (EXIT_FAILURE);
 	}
-	printf("%s\n", argv[1]);
-	return (0);
+
+
+	for (i = 0; (read = getline(&line, &len, fp)) != -1; i++)
+	{
+		if (line[0] == '\n' || line[0] == '#')
+		{
+			printf("Skipped\n");
+			continue;
+		}
+		opCode = strtok(line, "\t \n");
+		if (!opCode)
+			continue;
+		for (j = 0, opExists = 0; instructions[j].opcode != NULL; j++)
+		{
+			if (strcmp(opCode, instructions[j].opcode) == 0)
+			{
+				opExists = 1;
+				printf("Exists\n");
+				break;
+			}
+		}
+		if (!opExists)
+		{
+			fprintf(stderr, "L%lu: unknown instruction %s\n", i, opCode);
+			return (EXIT_FAILURE);
+		}
+	}
+	printf("Successfully Read: %s\n", argv[1]);
+	free(line);
+	fclose(fp);
+	return (EXIT_SUCCESS);
 }
