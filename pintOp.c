abd55c53b629fcb5b7ba3244f6c61c70f2f64c8b
Mario-Mellek <mario.samy266@gmail.com>
2023-05-26 01:46:44 +0000

    added comments to pintOp

--- pintOp.c
+++ pintOp.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * pintOp - Prints the value at the top of the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: The line number of the file being processed.
+ */
+
 void pintOp(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
