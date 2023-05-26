83f43b23fbdc6e59fbb4416f71f253b8a358a59b
Mario-Mellek <mario.samy266@gmail.com>
2023-05-26 02:19:25 +0000

    Implemented the add opcode.

--- addOp.c
+++ addOp.c
@@ -1,5 +1,11 @@
 #include "monty.h"
 
+/**
+ * addOp - Adds the top two elements of the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: The line number of the file being processed.
+ */
+
 void addOp(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
